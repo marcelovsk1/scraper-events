1f4a2ab3703044843faa846175f8e34802149352
Marcelo Amaral Alves <marceloaa@live.com>
2024-05-16 13:42:39 -0300

    facebook location

--- webpages/facebook.py
+++ webpages/facebook.py
@@ -4,86 +4,79 @@ from bs4 import BeautifulSoup
 import time
 from geopy.geocoders import Nominatim
 import re
-from datetime import datetime
-
-
-def scroll_to_bottom(driver, max_scroll=1):
-    for _ in range(max_scroll):
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(2)
-
+from datetime import datetime, timedelta
+from geopy.exc import GeocoderTimedOut, GeocoderUnavailable
 
 def scroll_to_bottom(driver, max_scroll=1):
     for _ in range(max_scroll):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(2)
-
 
 def format_date(date_str):
     print("Original date string:", date_str)
-
-    # Verificar se a string da data está vazia
     if not date_str:
         print("Erro: String de data vazia.")
         return None
 
-    # Usar expressões regulares para extrair os componentes da data
-    pattern = r"(\w+) (\d+) AT (\d{1,2}:\d{2}\s*(?:AM|PM)) – (\w+) (\d+) AT (\d{1,2}:\d{2}\s*(?:AM|PM)) EDT"
-    match = re.match(pattern, date_str)
-    if match:
-        start_month, start_day, start_time, end_month, end_day, end_time = match.groups()
+    patterns = [
+        (r"(\w+) (\d+) AT (\d{1,2}:\d{2}\s*(?:AM|PM)) – (\w+) (\d+) AT (\d{1,2}:\d{2}\s*(?:AM|PM)) (\d{4}) EDT", '%b'),
+        (r"(\w+), (\w+) (\d+), (\d{4}) AT (\d{1,2}:\d{2}\s*(?:AM|PM)) – (\d{1,2}:\d{2}\s*(?:AM|PM))", '%B')
+    ]
 
-        # Converter os nomes dos meses para seu equivalente numérico
-        start_month_num = datetime.strptime(start_month, '%b').month
-        end_month_num = datetime.strptime(end_month, '%b').month
+    for pattern, month_format in patterns:
+        match = re.match(pattern, date_str)
+        if match:
+            if len(match.groups()) == 7:
+                start_month, start_day, start_time, end_month, end_day, end_time, year = match.groups()
+            elif len(match.groups()) == 6:
+                day_of_week, start_month, start_day, year, start_time, end_time = match.groups()
+                end_day = start_day
+                end_month = start_month  # Use start_month as end_month for single-day events
 
-        # Formatar as datas de início e fim
-        formatted_start_date = f"{start_day}/{start_month_num:02d} at {start_time}"
-        formatted_end_date = f"{end_day}/{end_month_num:02d} at {end_time}"
+            start_date = datetime.strptime(f"{start_day} {start_month} {year} {start_time}", f"%d %B %Y %I:%M %p")
+            end_date = datetime.strptime(f"{end_day} {end_month} {year} {end_time}", f"%d %B %Y %I:%M %p")
 
-        print("Formatted start date:", formatted_start_date)
-        print("Formatted end date:", formatted_end_date)
+            if end_date <= start_date:
+                end_date += timedelta(days=1)
 
-        return (formatted_start_date, formatted_end_date)
-    else:
-        print("Erro: Formato de data inválido.")
-        return None
+            formatted_start_date = start_date.strftime("%d/%m/%Y at %H:%M")
+            formatted_end_date = end_date.strftime("%d/%m/%Y at %H:%M")
+            print("Formatted start date:", formatted_start_date)
+            print("Formatted end date:", formatted_end_date)
+            return (formatted_start_date, formatted_end_date)
 
-# Exemplo de uso:
-date_str = "MAY 31 AT 11:00 AM – JUN 1 AT 4:00 PM EDT"
-format_date(date_str)
-
+    print("Erro: Formato de data inválido.")
+    return None
 
 def get_coordinates(location):
-    geolocator = Nominatim(user_agent="event_scraper", timeout=30)  # Aumentando o tempo limite para 10 segundos
-    location = geolocator.geocode(location)
-    if location:
-        return location.latitude, location.longitude
-    else:
+    geolocator = Nominatim(user_agent="event_scraper", timeout=10)
+    full_location = f"{location}, Montreal, Quebec"
+    try:
+        location_obj = geolocator.geocode(full_location, exactly_one=True)
+        if location_obj:
+            return location_obj.latitude, location_obj.longitude
+        else:
+            print(f"No coordinates found for {location}. Trying fallback...")
+            location_obj = geolocator.geocode(location, exactly_one=True)
+            if location_obj:
+                return location_obj.latitude, location_obj.longitude
+            else:
+                return None, None
+    except Exception as e:
+        print(f"Error geocoding {location}: {e}")
         return None, None
 
 def open_google_maps(latitude, longitude):
-    google_maps_url = f"https://www.google.com/maps/search/?api=1&query={latitude},{longitude}"
-    return google_maps_url
+    return f"https://www.google.com/maps/search/?api=1&query={latitude},{longitude}"
 
 def get_location_details(latitude, longitude):
-
+    geolocator = Nominatim(user_agent="event_scraper")
     try:
-        geolocator = Nominatim(user_agent="event_scraper")
         location = geolocator.reverse((latitude, longitude), exactly_one=True)
         if location:
             address = location.address
             city = location.raw.get('address', {}).get('city')
             country_code = location.raw.get('address', {}).get('country_code')
-
-            # Ajuste para Montreal e ca apenas se ambos forem None
-            if city is None and country_code is None:
-                city = 'Montreal'
-                country_code = 'ca'
-            elif city and city.lower() == 'montreal':
-                city = 'Montreal'
-                country_code = 'ca'
-
             return address, city, country_code
         else:
             return None, None, None
@@ -91,18 +84,12 @@ def get_location_details(latitude, longitude):
         print(f"An error occurred while fetching location details: {e}")
         return None, None, None
 
-def scrape_facebook_events(driver, url, selectors, max_scroll=3):
-    global event_id_counter  # Referencing the global variable
-
+def scrape_facebook_events(driver, url, selectors, max_scroll=30):
     driver.get(url)
-    driver.implicitly_wait(20)
-
+    driver.implicitly_wait(20)  # Wait for elements to load
     all_events = []
     unique_event_titles = set()
-
-    # Scroll down to load more events
     scroll_to_bottom(driver, max_scroll)
-
     page_content = driver.page_source
     webpage = BeautifulSoup(page_content, 'html.parser')
     events = webpage.find_all(selectors['event']['tag'], class_=selectors['event'].get('class'))
@@ -113,30 +100,27 @@ def scrape_facebook_events(driver, url, selectors, max_scroll=3):
             continue
 
         event_url = 'https://www.facebook.com' + event_link['href'] if event_link['href'].startswith('/') else event_link['href']
-
         driver.get(event_url)
-        time.sleep(1)
-
+        time.sleep(1)  # Allow event page to load
         event_page_content = driver.page_source
         event_page = BeautifulSoup(event_page_content, 'html.parser')
 
         event_title_elem = event_page.find('span', class_='x1lliihq x6ikm8r x10wlt62 x1n2onr6')
         if event_title_elem:
             event_title = event_title_elem.text.strip()
-            if any(event_title == existing_title for existing_title in unique_event_titles):
+            if event_title in unique_event_titles:
+                driver.back()
                 continue
+            unique_event_titles.add(event_title)
         else:
+            driver.back()
             continue
 
         description = event_page.find('div', class_='xdj266r x11i5rnm xat24cr x1mh8g0r x1vvkbs').text.strip() if event_page.find('div', class_='xdj266r x11i5rnm xat24cr x1mh8g0r x1vvkbs') else None
-
-        location_div = event_page.find('div', class_='x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz xt0b8zv xzsf02u x1s688f')
-        location_span = event_page.find('span', class_='xt0psk2')
-        location_text = location_div.text.strip() if location_div else (location_span.text.strip() if location_span else None)
-
+        location_div = event_page.find('div', class_='x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x1sur9pj xkrqix3 xzsf02u x1s688f')
+        location_text = location_div.text.strip() if location_div else None
         latitude, longitude = get_coordinates(location_text)
         google_maps_url = open_google_maps(latitude, longitude)
-        event_id_counter = 0
 
         address_span = event_page.find('span', class_='x193iq5w xeuugli x13faqbe x1vvkbs xlh3980 xvmahel x1n0sxbx x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x4zkp8e x3x7a5m x1f6kntn xvq8zen xo1l8bm xi81zsa x1yc453h')
         address = address_span.text.strip() if address_span else None
@@ -220,14 +204,14 @@ if __name__ == "__main__":
     for source in sources:
         print(f"Scraping events from: {source['name']}")
         if source['name'] == 'Facebook':
-            events = scrape_facebook_events(driver, source['url'], source['selectors'])  # Corrected incomplete statement
+            events = scrape_facebook_events(driver, source['url'], source['selectors'])
             if events is not None:
-                for event in events:
-                    if 'Div' in event['Location']:
-                        event['Location']['Div'] = str(event['Location']['Div'])
-                    if 'Span' in event['Location']:
-                        event['Location']['Span'] = str(event['Location']['Span'])
-
                 all_events.extend(events)
             else:
-     
+                print("No events found.")
+
+    # Save events to JSON file
+    with open('facebook.json', 'w') as f:
+        json.dump(all_events, f, indent=4)
+
+    driver.quit()
